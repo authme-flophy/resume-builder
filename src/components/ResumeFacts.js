b9a435de993abfb8a946970826da534f90e3fe4e
Waikau Maina <ericmaina.dev@gmail.com>
2022-10-07 02:21:46 +0300

    added classes for the resume facts

--- src/components/ResumeFacts.js
+++ src/components/ResumeFacts.js
@@ -3,20 +3,20 @@ import React from 'react'
 function ResumeFacts() {
   return (
     <div className='resume-facts'>
-      <h1>Here Are Some Facts About Resume\'s:</h1>
-      <ul>
-      <li>Recruiters spend 5 to 7 seconds on average going over resumes.</li>
-<li>76% of the resumes are rejected due to an unprofessional email address.</li>
-<li>88% of the resumes are rejected due to a picture on the resume</li>
-<li>79 % of job seekers believe they are likely to use social media in their job hunt, with 86 % of younger job searchers saying they are likely to use it.</li>
-<li>On average, 250 resumes are sent for each corporate job opening. Only 4 of those are called up for an interview and finally only one will be selected.</li>
-<li>Most of the resumes are sent digitally via mail [ about 90 %]</li>
-<li>A single typo on your resume cantake your dream job away. Recruiters will discard a resume if it has mistakes, according to 61% of them. Applicants with spelling problems will be disqualified by 43% of hiring supervisors.</li>
-<li>The percentage of resumes and job applications with inaccuracies is 53%</li>
-<li>70% of college students surveyed said they would put false information on a resume to land a job.</li>
-<li>Recruiters use LinkedIn to screen applicants in 87 percent of cases.</li>
-<li>Employers prefer applicants with job experience 91 percent of the time, and 65 percent of the time, they prefer candidates with relevant work experience.</li>
-<li>45 percent of the total  of the workforce are Millennials now, making them the most well-represented generation.</li>
+      <h1>Here Are Some Facts About Resume's:</h1>
+      <ul className='collection'>
+      <li className='collection-item'>Recruiters spend 5 to 7 seconds on average going over resumes.</li>
+<li className='collection-item'>76% of the resumes are rejected due to an unprofessional email address.</li>
+<li className='collection-item'>88% of the resumes are rejected due to a picture on the resume</li>
+<li className='collection-item'>79 % of job seekers believe they are likely to use social media in their job hunt, with 86 % of younger job searchers saying they are likely to use it.</li>
+<li className='collection-item'>On average, 250 resumes are sent for each corporate job opening. Only 4 of those are called up for an interview and finally only one will be selected.</li>
+<li className='collection-item'>Most of the resumes are sent digitally via mail [ about 90 %]</li>
+<li className='collection-item'>A single typo on your resume cantake your dream job away. Recruiters will discard a resume if it has mistakes, according to 61% of them. Applicants with spelling problems will be disqualified by 43% of hiring supervisors.</li>
+<li className='collection-item'>The percentage of resumes and job applications with inaccuracies is 53%</li>
+<li className='collection-item'>70% of college students surveyed said they would put false information on a resume to land a job.</li>
+<li className='collection-item'>Recruiters use LinkedIn to screen applicants in 87 percent of cases.</li>
+<li className='collection-item'>Employers prefer applicants with job experience 91 percent of the time, and 65 percent of the time, they prefer candidates with relevant work experience.</li>
+<li className='collection-item'>45 percent of the total  of the workforce are Millennials now, making them the most well-represented generation.</li>
       </ul>
     </div>
   )
